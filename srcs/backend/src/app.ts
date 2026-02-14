a13675ebe4a6ac18b35f34ca597b450e755ed764
Rivo Vonjiniaina Hanitrarivelo <rhanitra@c1r10s7.42antananarivo.mg>
2026-02-14 15:17:30 +0300

    pgadmin

--- srcs/backend/src/app.ts
+++ srcs/backend/src/app.ts
@@ -3,12 +3,15 @@ import cors from "cors"
 
 const app: Express = express()
 
+// MIDDLEWARES
+
+app.use(express.json())
+
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true,
 }))
 
-app.use(express.json())
 
 app.get("/api/health", (_req, res) => {
   res.json({ status: "ok" })
---
+++ srcs/frontend/src/cours.md
@@ -0,0 +1,1 @@
+https://www.youtube.com/watch?v=TYB-Lz8YGFk&t=23s
